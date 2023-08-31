f19d906e463c90b149df8c8c1dcfc414bce36237
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-09-01 02:17:57 +0300

    tasks 1,2,3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { Profile } from "./profile/profile";
+import user from '../user.json'
+import { Statistics } from "./statistics/Statistics";
+import statistics from '../statistics.json'
+import { Friends } from "./friends/friends";
+import friends from '../friends.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile 
+        username={user.username} 
+        tag={user.tag} 
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+
+      <Statistics 
+      title={'Title'}
+      Roma={statistics}
+      />
+
+      <Friends 
+      friends={friends}
+      />
     </div>
   );
 };
